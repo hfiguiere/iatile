8348fe1c76a9e42e85b8e5d7f8daac4bbb2056f3
Hubert Figuière <hub@figuiere.net>
2019-03-10 12:20:57 -0400

    Better error handling. Accept URL as ID

--- settile.py
+++ settile.py
@@ -2,37 +2,51 @@
 
 # Not Python 3????
 
+import string
 import sys
 import argparse
 from xml.dom import minidom
-from internetarchive import download, get_item, get_files
+from internetarchive import get_item
 
 URL_BASE = "https://archive.org/details/"
 
 parser = argparse.ArgumentParser(description="Set tile for Internet Archive item")
-parser.add_argument("item_id", help="Item ID")
+parser.add_argument("item_id", help="Item ID or URL")
 parser.add_argument("-n", "--dry-run", action="store_true", help="Dry run")
 parser.add_argument("-p", type=int, help="The page to set as tile (default=0)", default=0)
 
 args = parser.parse_args()
 
-item = get_item(args.item_id)
+item_id = args.item_id
+if string.find(item_id, URL_BASE) == 0:
+    item_id = string.replace(item_id, URL_BASE, "")
 
-fnames = [f.name for f in get_files(args.item_id, glob_pattern='*_scandata.xml')]
-print(fnames)
+item = get_item(item_id)
+if item is None:
+    print("Item not found.")
+    sys.exit(1)
+
+fnames = [f.name for f in item.get_files(glob_pattern='*_scandata.xml')]
 if len(fnames) != 1:
     print("Too many scandata, abort.")
     sys.exit(1)
 
 scandata_file = fnames[0]
 
-if download(args.item_id, scandata_file, no_directory=True) is False:
+if item.download(scandata_file, no_directory=True) is False:
     print("Download failed")
     sys.exit(1)
 
 modified = False
 xmldoc = minidom.parse(scandata_file)
+if xmldoc is None:
+    print("Couldn't parse scandata file")
+    sys.exit(1)
+
 pages = xmldoc.getElementsByTagName("page")
+if len(pages) == 0:
+    print("No pages found")
+    sys.exit(1)
 
 for page in pages:
     pageNum = int(page.attributes["leafNum"].value)
